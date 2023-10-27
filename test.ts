24ddab50cd7e5b66a064eabce30d109384cb3875
takaodaze <gin2798@gmail.com>
2023-10-28 00:53:26 +0900

    test 追加

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.203.0/assert/mod.ts";
 import { bubbleSort } from "./sort/bubble.ts";
 import { selectionSort } from "./sort/selection.ts";
 import { insertionSort } from "./sort/insertion.ts";
-import { mergeSort } from "./sort/merge.ts";
+import { mergeSort, recurciveMergeSort } from "./sort/merge.ts";
 
 const rawTestDataJson = Deno.readTextFileSync("./test_data.json");
 
@@ -33,5 +33,11 @@ Deno.test(function insertion() {
 Deno.test(function merge() {
   for (let i = 0; i < testdata.length; i++) {
     assertEquals(mergeSort(testdata[i]), sortedTestdata[i]);
+  }
+});
+
+Deno.test(function recurciveMerge() {
+  for (let i = 0; i < testdata.length; i++) {
+    assertEquals(recurciveMergeSort(testdata[i]), sortedTestdata[i]);
   }
 });
